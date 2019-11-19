7487f7b23925566314a36cb988fadabb23e0f0be
x101x <1642372486@qq.com>
2019-11-19 22:45:39 +0800

    deepclone

---
+++ interview/deepclone/index.js
@@ -0,0 +1,21 @@
+// let a = 'ss'
+// let b = a;
+// console.log(b);
+// a='ww'
+// console.log(a);
+// console.log(b);
+
+let a = {
+  name: 'wn',
+  book: {
+    title: "you w",
+    price: "45w"
+  }
+}
+
+let b = a 
+
+console.log(b);
+a.name = 'n'
+console.log(a);
+console.log(b);
